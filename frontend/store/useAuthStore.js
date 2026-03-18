e7c0831dc45a325f0c1c48e943874f9f87ac3b18
Nitya Shah <nitya@bacancy.com>
2026-03-18 13:30:41 +0530

    fix: fixed socket issue

--- frontend/store/useAuthStore.js
+++ frontend/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from '../src/lib/axios.js'
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.BASE_URL;
 export const useAuthStore = create((set, get) => ({ //set is a function provided by Zustand to update the state.
     authUser: null,
     isSigningUp: false,
